a0f6f9ad8fe1c5a3cc411128c444e81889a94a16
Diogo <diogotibirica@ufl.edu>
2025-11-16 22:18:53 -0500

    Added all unit tests

--- test_calculator.py
+++ test_calculator.py
@@ -7,7 +7,7 @@ import calculator
 
 class TestCalculator(unittest.TestCase):
 
-def test_add(self):
+    def test_add(self):
         self.assertEqual(calculator.add(2, 3), 5)
         self.assertEqual(calculator.add(-1, 1), 0)
         self.assertEqual(calculator.add(-5, -5), -10)
@@ -55,10 +55,10 @@ def test_add(self):
         self.assertAlmostEqual(calculator.hypotenuse(-3, 4), 5)
 
     def test_sqrt(self):
-        self.assertAlmostEqual(calculator.sqrt(4), 2)
-        self.assertAlmostEqual(calculator.sqrt(144), 12)
+        self.assertAlmostEqual(calculator.square_root(4), 2)
+        self.assertAlmostEqual(calculator.square_root(144), 12)
         with self.assertRaises(ValueError):
-            calculator.sqrt(-1)
+            calculator.square_root(-1)
 # Do not touch this
 if __name__ == "__main__":
     unittest.main()
