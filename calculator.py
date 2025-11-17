72d5a92e1af7109bbc8a80d197c51f9a54a7a895
Diogo <diogotibirica@ufl.edu>
2025-11-16 21:56:56 -0500

    Partner 2 functions

--- calculator.py
+++ calculator.py
@@ -1,11 +1,33 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot calculate square root of a negative number")
+    return math.sqrt(a)
 
+def hypotenuse(a, b):
+    return math.hypot(a, b)
 
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
+
+def logarithm(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError("Logarithm base must be greater than 0 and not 1")
+    if b <= 0:
+        raise ValueError("Logarithm argument must be greater than 0")
+    return math.log(b, a)
+
+def exponent(a, b):
+    return a ** b
